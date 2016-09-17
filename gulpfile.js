a720d493c82848aefadb8eeda61f89a6f4124f76
Максим <malahof.maxim@yandex.ru>
2016-09-17 16:19:55 +0400

    Настроил создание спрайтов. Теперь css и sprite помещаютсяя в разные папки.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
-var gulp = require("gulp");
-var spritesmith = require("gulp.spritesmith");
+var gulp = require('gulp');
+var spritesmith = require('gulp.spritesmith');
 
-gulp.task("sprite", function () {
-  var spriteData = gulp.src("images/icons/**/*.png").pipe(spritesmith({
-    imgName: "sprite.png",
-    cssName: "sprite.css"
+gulp.task('sprite', function () {
+	var spriteData = gulp.src("images/icons/**/*.png")
+	.pipe(spritesmith({
+		imgName: 'sprite.png',
+		cssName: 'sprite.css'
   }));
-  return spriteData.pipe(gulp.dest("sprite-images/"));
-});
 
+  var imgStream = spriteData.img
+    .pipe(gulp.dest("sprite-images/"));
+
+  var cssStream = spriteData.css
+    .pipe(gulp.dest("sprite-css/"));
+  return;
+});
