378373c79fe7115b09463f21070f72f6799e82ee
Максим <malahof.maxim@yandex.ru>
2016-09-24 23:03:01 +0400

    Редактирование Readme и gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
-var gulp = require('gulp');
-var spritesmith = require('gulp.spritesmith');
+var gulp = require('gulp'),
+	spritesmith = require('gulp.spritesmith');
 
 gulp.task('sprite', function () {
-	var spriteData = gulp.src("images/icons/**/*.png")
+	var spriteData = gulp.src("dev/images/icons/*.png")
 	.pipe(spritesmith({
 		imgName: 'sprite.png',
 		cssName: 'sprite.less'
-  }));
+	}));
 
-  var imgStream = spriteData.img
-    .pipe(gulp.dest("sprite-images/"));
+	var imgStream = spriteData.img
+		.pipe(gulp.dest("dev/images/icons/sprite/"));
 
-  var cssStream = spriteData.css
-    .pipe(gulp.dest("sprite-less/"));
-  return;
-});
+	var cssStream = spriteData.css
+		.pipe(gulp.dest("dev/css/helpers/"));
+	return;
+	});
