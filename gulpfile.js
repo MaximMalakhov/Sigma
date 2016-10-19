300b083e1e898caee8ac39a97c07d49ca933f495
Максим <malahof.maxim@yandex.ru>
2016-10-19 20:59:40 +0400

    Рефакторинг css: -Добавлено адаптивное меню для телофонов -Изменены изображения товаров -Акция заменена на текст+фон

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ var gulp = require('gulp'),
 gulp.task('sprite', function () {
 	var spriteData = gulp.src("dev/images/icons/*.png")
 	.pipe(spritesmith({
-		imgName: 'sprite.png',
-		cssName: 'sprite.less'
+		imgName: 'icons.png',
+		cssName: 'icons.less'
 	}));
 
 	var imgStream = spriteData.img
